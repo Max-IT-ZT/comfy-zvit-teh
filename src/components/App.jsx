852a6329971ceb755851ce3da5daaa626682abef
Max-IT-ZT <babuskomax@gmail.com>
2024-01-20 23:22:37 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from 'react';
 import './App.css';
+
+import React, { useState, useEffect } from 'react';
 
 const App = () => {
   const [salesData, setSalesData] = useState(() => {
