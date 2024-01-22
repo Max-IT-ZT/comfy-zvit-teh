e8944d31cbecb36cb516d3529315e01fde6bffc3
Max-IT-ZT <babuskomax@gmail.com>
2024-01-22 23:07:49 +0200

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,5 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 
 const sendTelegramMessage = text => {
   const botToken = '6892704120:AAFGsWOgtcMFv1F3TZcO2YeRNK0nfGbKCTQ';
